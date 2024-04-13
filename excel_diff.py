d52164b2e316b39a25c57f89deee9393671ced6a
woogyeom <woogyeom99@gmail.com>
2024-04-13 18:43:59 -0400

    Debugging mostly

--- excel_diff.py
+++ excel_diff.py
@@ -1,11 +1,23 @@
 import pandas as pd
 
-# 엑셀 파일 읽기
-df1 = pd.read_excel('result1.xlsx')
-df2 = pd.read_excel('result2.xlsx')
+df1 = pd.read_excel('GooglePlaystore_afterprep5.xlsx')
+df2 = pd.read_excel('GooglePlaystore_cleaned5.xlsx')
 
-# 두 데이터프레임 비교
-diff = df1.compare(df2)
+# in_df1_not_in_df2 = df1[~df1.index.isin(df2.index)]
+# in_df2_not_in_df1 = df2[~df2.index.isin(df1.index)]
 
-# 차이점 출력
-print(diff)
+# print("Rows that only I have:")
+# print(in_df1_not_in_df2)
+
+# print("\nRows that only Jasmit has:")
+# print(in_df2_not_in_df1)
+
+for idx, row1 in df1.iterrows():
+    row2 = df2.iloc[idx]
+    if not row1.equals(row2):
+        print("File 1:")
+        print(row1)
+        print()
+        print("File 2:")
+        print(row2)
+        break
